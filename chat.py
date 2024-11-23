dc595101264da393756c191c0d33dd16a332065c
black940514 <xodus4354@gmail.com>
2024-11-23 23:45:10 +0900

    llm_application_1123

---
+++ chat.py
@@ -0,0 +1,58 @@
+# import streamlit as st
+# print(1)
+# st.set_page_config(page_title="소득세 챗봇", page_icon="🇰🇷")
+# print(2)
+# st.title("🇰🇷 소득세 챗봇")
+# print(3)
+# st.caption("소득세 관련 질문을 물어보세요!")
+
+# if 'message_list' not in st.session_state:
+#     st.session_state['message_list'] = []
+# print(4)
+
+# for message in st.session_state['message_list']:
+#     with st.chat_message(message['role']):
+#         st.write(message['content'])
+
+# if user_question := st.chat_input(placeholder="소득세 질문을 입력하세요!"):
+#     with st.chat_message("user"):
+#         st.write(user_question)
+#     st.session_state['message_list'].append({"role": "user", "content": user_question})
+
+
+
+import streamlit as st
+
+
+from dotenv import load_dotenv
+
+
+from llm import get_ai_response
+
+st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
+
+st.title("🤖 소득세 챗봇")
+st.caption("소득세에 관련된 모든것을 답해드립니다!")
+
+load_dotenv()
+
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+for message in st.session_state.message_list:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+
+
+if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 내용들을 말씀해주세요!"):
+    with st.chat_message("user"):
+        st.write(user_question)
+    st.session_state.message_list.append({"role": "user", "content": user_question})
+
+    with st.spinner("답변을 생성하는 중입니다"):
+        ai_response = get_ai_response(user_question)
+        with st.chat_message("ai"):
+            ai_message = st.write_stream(ai_response)
+            st.session_state.message_list.append({"role": "ai", "content": ai_message})
